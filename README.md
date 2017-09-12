68051278f7072aabd0bc31a66f8fa342ba3e3966
SkyLothar <allothar@gmail.com>
2017-09-13 01:23:56 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+喜新厌旧
+--------
+
+### 安装
+
+```
+pip install -r requirements.txt
+```
+
+### 生成bash命令
+使用 DNSPOD相关配置及AliCDN相关配置获取需要更新的域名信息, 生成脚本文件等待执行
+
+```
+python generate.py $DNSPOD_EMAIL $DNSPOD_TOKEN $CDN_ACCESS_ID $CDN_ACCESS_TOKEN $BASH_OUTPUT
+```
+
+
+### 执行bash命令
+执行上一步生成的脚本文件
+
+```
+bash $BASH_OUTPUT
+```
---
+++ generate.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import click
+import tempfile
+
+from certbot_aliyun_cdn.client import CDN
+
+
+def write_config_ini(email, token, access_key_id, access_key_secret, output):
+    content = [
+        ("certbot_dns_dnspod:dns_dnspod_email", email),
+        ("certbot_dns_dnspod:dns_dnspod_api_token", token),
+        ("certbot_alicdn:aliyun_cdn_access_key_id", access_key_id),
+        ("certbot_alicdn:aliyun_cdn_access_key_secret", access_key_secret)
+    ]
+    for key, val in content:
+        output.write("{0} = '{1}'\n".format(key, val))
+
+
+def bash_for_domain(config, domain):
+    return (
+        "certbot run "
+        "--authenticator certbot-dns-dnspod:dns-dnspod "
+        "--certbot-dns-dnspod:dns-dnspod-credentials {config} "
+        "--reinstall --redirect "
+        "--installer certbot-aliyun-cdn:aliyun-cdn "
+        "--certbot-aliyun-cdn:aliyun-cdn-credentials {config} "
+        "--domain {domain}\n"
+    ).format(config=config, domain=domain)
+
+
+@click.command()
+@click.argument("email")
+@click.argument("token")
+@click.argument("access-key-id")
+@click.argument("access-key-secret")
+@click.argument("output", type=click.File("wt"))
+def generate(email, token, access_key_id, access_key_secret, output):
+    tmp = tempfile.NamedTemporaryFile("wt", delete=False)
+    with tmp as f:
+        write_config_ini(email, token, access_key_id, access_key_secret, f)
+
+    output.write("#!/bin/bash\nset -e\n")
+
+    cdn = CDN()
+    cdn.set_credentials(access_key_id, access_key_secret)
+    for domain in cdn.list_domains():
+        if cdn.check_expiratoin(domain):
+            output.write(bash_for_domain(tmp.name, domain))
+
+
+if __name__ == "__main__":
+    generate()
